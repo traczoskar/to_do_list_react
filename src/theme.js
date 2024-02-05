2e0cdaf9a4023ca9576c127f0e499febce47ceb2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-05 15:32:04 +0100

    added theme file

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    alabaster: "#f8f8f8",
+    silver: "#cccccc",
+    darksilver: "#bbbbbb",
+    mercury: "#e8e8e8",
+    mineshaft: "#333333",
+    emperor: "#555555",
+    boulder: "#777777",
+    black: "#000000",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};

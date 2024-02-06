b958fa82085b54b11eb2d41b642e02b68371f199
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-06 14:48:18 +0100

    corrected the Containter component for styled-component file

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
-const Container = ({ className, children }) => (
-  <main className={className}>{children}</main>
-);
-
-export const StyledContainer = styled(Container)`
+export const Container = styled.main`
   max-width: 900px;
   margin: 0 auto;
   padding: 20px;

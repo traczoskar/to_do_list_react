440b34541efa494e7e912ac751b131b86d281bc2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 15:39:31 +0100

    created new Redux store and applied it and Provider into entrypoint file

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyle.js";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme.js";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 

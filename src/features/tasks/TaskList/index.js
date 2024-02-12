05c26d22d08c797a94760720447baf0d3bb68637
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 15:42:46 +0100

    changed TaskList component logic with Redux state menagment convention

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,19 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
 import { List, Task, Button, Content } from "./styled.js";
+import { selectTasks, toggleTaskDone, removeTask } from "../tasksSlice.js";
 
-const TaskList = ({ taskList, hideDone, removeTask, toggleTaskDone }) => (
-  <List>
-    {taskList.map((task) => (
-      <Task hidden={task.done && hideDone} key={task.id}>
-        <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
-          {task.done ? "✓" : ""}
-        </Button>
-        <Content done={task.done}>{task.content}</Content>
-        <Button remove onClick={() => removeTask(task.id)}>
-          🗑️
-        </Button>
-      </Task>
-    ))}
-  </List>
-);
+const TaskList = () => {
+  const { taskList, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
+    <List>
+      {taskList.map((task) => (
+        <Task hidden={task.done && hideDone} key={task.id}>
+          <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
+            {task.done ? "✓" : ""}
+          </Button>
+          <Content done={task.done}>{task.content}</Content>
+          <Button remove onClick={() => dispatch(removeTask(task.id))}>
+            🗑️
+          </Button>
+        </Task>
+      ))}
+    </List>
+  );
+};
 
 export default TaskList;

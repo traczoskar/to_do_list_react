e83e95759f9305d970d3ba1eff9f6ea73257929c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 15:42:23 +0100

    changed Buttons component logic with Redux state menagment convention

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,22 +1,28 @@
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 import { Button, ButtonsWrapper } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 
-const Buttons = ({ taskList, hideDone, toggleHideDone, setAllDone }) => (
-  <ButtonsWrapper>
-    {taskList.length > 0 && (
-      <>
-        <Button onClick={toggleHideDone}>
-          {`${hideDone ? "Pokaż" : "Ukryj"}`} ukończone
-        </Button>
-        <Button
-          onClick={setAllDone}
-          className="buttons__button"
-          disabled={taskList.every(({ done }) => done)}
-        >
-          Ukończ wszystkie
-        </Button>
-      </>
-    )}
-  </ButtonsWrapper>
-);
+const Buttons = () => {
+  const { taskList, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+
+  return (
+    <ButtonsWrapper>
+      {taskList.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {`${hideDone ? "Pokaż" : "Ukryj"}`} ukończone
+          </Button>
+          <Button
+            onClick={() => dispatch(setAllDone())}
+            disabled={taskList.every(({ done }) => done)}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </ButtonsWrapper>
+  );
+};
 
 export default Buttons;

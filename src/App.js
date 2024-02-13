00b96d9b962f1e3f5390e241b399289f78a814e8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-13 15:52:44 +0100

    created first routing template in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
+import { HashRouter, Route, Link, NavLink, Switch } from "react-router-dom";
 
-export default () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);

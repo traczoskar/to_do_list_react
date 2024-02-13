f197274a7d906e4355c3e47380e2fec04d2ba22e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-13 12:47:01 +0100

    used <Button /> component in Form section to dispatch fetching example tasks from JSON file using Redux-Saga

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,15 +1,32 @@
 import Form from "./Form";
 import TaskList from "./TaskList";
-import Buttons from "./Buttons";
+import Buttons from "../../Buttons";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "../../Buttons/styled.js";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "../../common/Container/styled";
+import { fetchExampleTasks, selectLoading } from "./tasksSlice.js";
 
 function Tasks() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+        extraHeaderContent={
+          <Button
+            loading={loading}
+            onClick={() => dispatch(fetchExampleTasks())}
+          >
+            {loading ? "Ładowanie zadań..." : "Pobierz przykładowe zadania"}
+          </Button>
+        }
+      />
       <Section
         title="Lista zadań"
         body={<TaskList />}

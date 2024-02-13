129cf8ce695dcdf6c822eb042308c598ca1f5f9e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-13 12:38:29 +0100

    added fetching function for default tasks from exampleTasks.json file

---
+++ src/features/tasks/getExampleTasks.js
@@ -0,0 +1,9 @@
+export const getExampleTasks = async () => {
+  const response = await fetch("/to_do_list_react/exampleTasks.json");
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return await response.json();
+};

e8e94fb58b4621afddd3e4c2ad339ec18a19cca0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-13 12:39:19 +0100

    implemented Saga middleware in store.js and added rootSaga file

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import taskListReducer from "./features/tasks/tasksSlice";
+import rootSaga from "./rootSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
   reducer: {
     taskList: taskListReducer,
   },
+  middleware: () => [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
+
+export default store;

31d61ca0092522895a2269c251487eda87e41a22
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-14 01:01:56 +0100

    added 2 new selectors fo searching feature to use

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -55,5 +55,16 @@ export const selectIsTaskListEmpty = (state) => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = (state) =>
   selectTasks(state).every(({ done }) => done);
 export const selectLoading = (state) => selectTasksState(state).loading;
+export const getTaskById = (state, taskId) =>
+  selectTasks(state).find(({ id }) => id === taskId);
+export const selectTasksByQuery = (state, query) => {
+  const taskList = selectTasks(state);
+  if (!query || query.trim() === "") {
+    return taskList;
+  }
+  return taskList.filter(({ content }) =>
+    content.toUpperCase().includes(query.trim().toUpperCase())
+  );
+};
 
 export default tasksSlice.reducer;

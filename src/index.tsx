b80b3bc5e6ecda79668f150dce143208bf0516a8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-12 15:39:57 +0100

    changed entry point file for .tsx format

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { theme } from "./theme.js";
 import { Provider } from "react-redux";
 import store from "./store.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
